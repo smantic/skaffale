c8a627f7926a1c39db6c6ccd661a61aa015022c9
lukfd <luca.comba98@gmail.com>
2023-08-21 07:32:30 +0200

    Change lister to skaffale

--- client/src/components/Footer/Footer.vue
+++ client/src/components/Footer/Footer.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div>
-        <a href="https://github.com/lukfd/lister"><i class="pi pi-github px-2"></i></a>
+        <a href="https://github.com/lukfd/skaffale"><i class="pi pi-github px-2"></i></a>
         <a href="https://twitter.com/luca_comba"><i class="pi pi-twitter px-2"></i></a>
     </div>
 </template>
